081ce9ba78949e013c7507e0adae047a9df155d5
afrizalars <mohammad.afrizal@mail.ugm.ac.id>
2022-03-04 11:47:02 +0700

    ..'

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.get('/', (req, res) => {
   const name = process.env.NAME || 'World';
-  res.send(`Hello ${name}!`);
+  res.send(`Hello 2 ${name}!`);
 });
 
 const port = parseInt(process.env.PORT) || 8080;
